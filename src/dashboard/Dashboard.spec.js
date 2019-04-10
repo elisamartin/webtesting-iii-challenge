81bff38d2399639146193eaeb056d7765ef562ba
elisamartin <elisamv93@gmail.com>
2019-04-10 17:39:37 +0200

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
 // Test away
+import React from 'react';
+import * as rt from 'react-testing-library';
+import Dashboard from './Dashboard';
+
+describe('Dashboard component', () => {
+	it('Dashboard renders', () => {
+		rt.render(<Dashboard />);
+	});
+});
